f40543b9300c7f03e46ccf56369c2efc0eabbba0
JesusLX <jpmx96@gmail.com>
2020-04-19 15:40:02 +0200

    Que me quema!

--- Assets/Scripts/FireController.cs
+++ Assets/Scripts/FireController.cs
@@ -6,6 +6,12 @@ public class FireController : Singleton<FireController> {
     public LayerMask layerMask;
     public float fireForce;
     public bool burning;
+    public ParticleSystem parentPS;
+    public List<ParticleSystem> childsPS;
+    float maxRadius = .4f;
+    float maxParticlesEmision = 30;
+
+
     private void Start() {
         StartCoroutine(Fire());
     }
@@ -22,8 +28,17 @@ public class FireController : Singleton<FireController> {
             fireForce = force;
             if (fireForce <= 0) {
                 burning = false;
+                parentPS.Stop();
             } else {
                 burning = true;
+                if (!parentPS.isPlaying)
+                    parentPS.Play();
+                foreach (ParticleSystem ps in childsPS) {
+                    var mEmision = ps.emission;
+                    var mShape = ps.shape;
+                    mEmision.rateOverTime = force;
+                    mShape.radius = force * maxRadius / maxParticlesEmision;
+                }
             }
 
             yield return new WaitForSeconds(.2f);
@@ -34,6 +49,6 @@ public class FireController : Singleton<FireController> {
         Gizmos.color = Color.red;
         //Draw the next cross position
         Gizmos.DrawWireCube(transform.position, transform.localScale);
-        
+
     }
 }
